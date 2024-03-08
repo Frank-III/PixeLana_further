2f48a8728713196d32ef36d1639959309403c506
Frank Wang <frankwaang@outlook.com>
2024-03-07 21:38:38 -0600

    feat(wip): rs backend

--- backend_rs/src/state.rs
+++ backend_rs/src/state.rs
@@ -28,6 +28,13 @@ pub struct Submission {
 }
 
 
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Content {
+  r#type: String,
+  content: String
+}
+
+
 #[derive(thiserror::Error, Debug)]
 pub enum GameError {
     #[error("Game is full")]
@@ -42,10 +49,6 @@ pub enum GameError {
     GameNotStarted,
     #[error("Game has already started")]
     GameStarted,
-    #[error("Game is over")]
-    GameOver,
-    #[error("Invalid player id")]
-    InvalidPlayerId,
     #[error("Invalid round")]
     InvalidRound
 }
@@ -84,10 +87,14 @@ impl GameState {
         if self.players.len() >= 8 {
             bail!(GameError::GameFull)
         };
+        if self.game_started {
+            bail!(GameError::GameStarted)
+        }
         self.players.push(player);
         return Ok(self.players.len() as u8);
     }
 
+    // TODO: add if is host check
     pub fn start_game(&mut self) -> Result<()> {
         if self.game_started {
             bail!(GameError::GameStarted)
@@ -108,21 +115,41 @@ impl GameState {
         round_imgs_or_prompts.insert(submission.player_id, submission.content);
 
         if round_imgs_or_prompts.len() as u8 == self.players.len() as u8 {
-          self.prev_round_imgs_or_prompts = Some(Mutex::new(rotate_map(round_imgs_or_prompts)))
+          self.prev_round_imgs_or_prompts = Some(Mutex::new(rotate_map(round_imgs_or_prompts)));
           return Ok(true); 
         }
 
         Ok(false)
     }
 
-    pub fn send_round_info() -> Result<()> {
-        Ok(())
+    pub fn send_round_info(&self, player_id:u8) -> Result<Content> {
+      let prev_state = self.prev_round_imgs_or_prompts.as_ref().ok_or(GameError::GameNotStarted)?.lock().unwrap();
+      let round_info = prev_state.get(&player_id).ok_or(GameError::PlayerDoesNotExist)?;
+
+      Ok({Content {
+        r#type: if self.round % 2 == 0 { "story".to_string() } else { "image".to_string() },
+        content: round_info.clone()
+      }})
     }
 
-    pub fn get_all_imgs_or_prompts(&self) -> Result<HashMap<u8, String>> {
-        let round_imgs_or_prompts = self.round_imgs_or_prompts.lock().unwrap();
-        let round_imgs_or_prompts = round_imgs_or_prompts.get(self.round as usize).unwrap();
-        Ok(round_imgs_or_prompts.clone())
+    pub fn get_all_imgs_or_prompts(&mut self, round: u8) -> Result<Vec<Content>> {
+      let round_imgs_or_prompts = self.round_imgs_or_prompts.lock().unwrap();
+      let round_img_or_prompt = (*round_imgs_or_prompts).iter().enumerate().map(|(idx, round_map)| {
+        Content {
+          r#type: if idx % 2 == 0 { "story".to_string() } else { "image".to_string() },
+          content: round_map.get(&round).unwrap().clone()
+        }
+      }).collect::<Vec<Content>>();
+      Ok(round_img_or_prompt)
     }
 
+    pub fn like_img(&mut self, player_id: u8, like_id: u8) ->Result<String> {
+      let round_imgs_or_prompts = self.round_imgs_or_prompts.lock().unwrap();
+      let img_vec_idx = if player_id > like_id { (player_id - like_id) % self.players.len() as u8 } else { (like_id - player_id) % self.players.len() as u8};
+      let best_img = round_imgs_or_prompts.get(img_vec_idx as usize).unwrap().get(&like_id).unwrap();
+      let mut leader_board = self.leader_board.lock().unwrap();
+      let player_score = leader_board.get_mut(&like_id).unwrap();
+      *player_score += 1;
+      Ok(best_img.clone())
+    }
 }
